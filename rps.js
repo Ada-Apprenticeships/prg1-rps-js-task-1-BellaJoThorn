8a3a4ef59ad263ac803779a4df76ada512b985c0
BellaJoThorn <bella.thorn@ada.ac.uk>
2024-10-16 11:59:27 +0000

    Working Commit 1

--- rps.js
+++ rps.js
@@ -1,22 +1,37 @@
-const readline = require("readline-sync")
 
-let player1Choice = readline.question("Player 1, choose rock, paper, or scissors!")
-let player2Choice = readline.question("Player 2, choose rock, paper, or scissors!")
+function rockPaperScissors(player1, player2) { //Function to run the rock, paper, scissors game
 
-function rockPaperScissors(player1, player2) {
+  return (player1 === player2) ? "draw" //Returns draw if the two variables passed are the same
 
-  return (player1 === player2) ? "draw"
-   : (player1 == "rock" && player2 == "scissors") ? "player1"
-   : (player1 == "scissors" && player2 == "paper") ? "player1"
-   : (player2 == "rock" && player1 == "scissors") ? "player2"
-   : (player2 == "scissors" && player1 == "paper") ? "player2"
-   : (player1 == "rock" && player2 == "paper") ? "player 2"
-   : (player2 == "rock" && player1 == "paper") ? "player1"
-   : "Please each choose rock, paper, or scissors";
+   : (player1 === "rock" && player2 === "scissors") ? "player1"   //Returns player1's wins for the basic game
+   : (player1 === "scissors" && player2 === "paper") ? "player1"  //Returns player1's wins for the basic game
+   : (player2 === "rock" && player1 === "paper") ? "player1"      //Returns player1's wins for the basic game
+
+   : (player2 === "rock" && player1 === "scissors") ? "player2"   //Returns player2's wins for the basic game
+   : (player2 === "scissors" && player1 === "paper") ? "player2"  //Returns player2's wins for the basic game
+   : (player1 === "rock" && player2 === "paper") ? "player2"      //Returns player2's wins for the basic game
+
+   : (player1 === "lizard" && player2 === "spock") ? "player1"    //Returns player1's wins for the advanced game
+   : (player1 === "spock" && player2 === "scissors") ? "player1"  //Returns player1's wins for the advanced game
+   : (player1 === "rock" && player2 === "lizard") ? "player1"     //Returns player1's wins for the advanced game
+   : (player1 === "paper" && player2 === "spock") ? "player1"     //Returns player1's wins for the advanced game
+   : (player1 === "scissors" && player2 === "lizard") ? "player1" //Returns player1's wins for the advanced game
+   : (player1 === "lizard" && player2 === "paper") ? "player1"    //Returns player1's wins for the advanced game
+   : (player1 === "spock" && player2 === "rock") ? "player1"      //Returns player1's wins for the advanced game
+
+   : (player2 === "lizard" && player1 === "spock") ? "player2"    //Returns player2's wins for the advanced game
+   : (player2 === "spock" && player1 === "scissors") ? "player2"  //Returns player2's wins for the advanced game   
+   : (player2 === "rock" && player1 === "lizard") ? "player2"     //Returns player2's wins for the advanced game
+   : (player2 === "paper" && player1 === "spock") ? "player2"     //Returns player2's wins for the advanced game
+   : (player2 === "scissors" && player1 === "lizard") ? "player2" //Returns player2's wins for the advanced game
+   : (player2 === "lizard" && player1 === "paper") ? "player2"    //Returns player2's wins for the advanced game
+   : (player2 === "spock" && player1 === "rock") ? "player2"      //Returns player2's wins for the advanced game
+
+   : "Please each choose rock, paper, scissors, lizard, or spock"; //Returns that incorrect variables have been passed
 
 }
 
-console.log(rockPaperScissors(player1Choice, player2Choice))
+console.log(rockPaperScissors("test","test"))
 
 
 
