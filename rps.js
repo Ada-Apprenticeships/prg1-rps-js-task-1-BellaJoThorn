5c028d2ccede61fe46462f6c5040b87552801cc0
BellaJoThorn <bella.thorn@ada.ac.uk>
2024-10-16 10:59:32 +0000

    First Commit

--- rps.js
+++ rps.js
@@ -1,10 +1,22 @@
+const readline = require("readline-sync")
+
+let player1Choice = readline.question("Player 1, choose rock, paper, or scissors!")
+let player2Choice = readline.question("Player 2, choose rock, paper, or scissors!")
+
 function rockPaperScissors(player1, player2) {
 
-  
-   
+  return (player1 === player2) ? "draw"
+   : (player1 == "rock" && player2 == "scissors") ? "player1"
+   : (player1 == "scissors" && player2 == "paper") ? "player1"
+   : (player2 == "rock" && player1 == "scissors") ? "player2"
+   : (player2 == "scissors" && player1 == "paper") ? "player2"
+   : (player1 == "rock" && player2 == "paper") ? "player 2"
+   : (player2 == "rock" && player1 == "paper") ? "player1"
+   : "Please each choose rock, paper, or scissors";
+
 }
 
-
+console.log(rockPaperScissors(player1Choice, player2Choice))
 
 
 
